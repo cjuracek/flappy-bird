b2974ff1806050fa4d00be910f3147931b11bd31
Cole Juracek <therealjuracek@gmail.com>
2024-01-14 17:37:19 -0500

    compliance with pre commit

--- flappy-bird/Driver.py
+++ flappy-bird/Driver.py
@@ -8,11 +8,15 @@ WHITE = (255, 255, 255)
 
 pygame.init()
 
-game_display = pygame.display.set_mode((GameConstants.DISPLAY_WIDTH, GameConstants.DISPLAY_HEIGHT))
-pygame.display.set_caption('Flappy Bird')
+game_display = pygame.display.set_mode(
+    (GameConstants.DISPLAY_WIDTH, GameConstants.DISPLAY_HEIGHT)
+)
+pygame.display.set_caption("Flappy Bird")
 clock = pygame.time.Clock()
 
-flappy_box = BoxClass.Box(GameConstants.DISPLAY_WIDTH * 0.25, GameConstants.DISPLAY_HEIGHT * 0.25)
+flappy_box = BoxClass.Box(
+    GameConstants.DISPLAY_WIDTH * 0.25, GameConstants.DISPLAY_HEIGHT * 0.25
+)
 
 delta_x = -15
 wall_x_positions = [x for x in range(GameConstants.DISPLAY_WIDTH, 0, delta_x)]
@@ -20,7 +24,10 @@ current_wall_position = 0
 top_wall_lengths = [x for x in range(50, 260, 30)]
 bottom_wall_lengths = [x for x in range(260, 50, -30)]
 index = random.randint(0, len(top_wall_lengths) - 1)
-top_wall_length, bottom_wall_length = top_wall_lengths[index], bottom_wall_lengths[index]
+top_wall_length, bottom_wall_length = (
+    top_wall_lengths[index],
+    bottom_wall_lengths[index],
+)
 
 score = 0
 while True:
@@ -32,15 +39,27 @@ while True:
     game_display.fill(WHITE)
 
     # Draw the floor
-    floor = pygame.draw.rect(game_display, BLACK, pygame.Rect((0, GameConstants.DISPLAY_HEIGHT * 0.92),
-                                                              (GameConstants.DISPLAY_WIDTH, 50)))
+    floor = pygame.draw.rect(
+        game_display,
+        BLACK,
+        pygame.Rect(
+            (0, GameConstants.DISPLAY_HEIGHT * 0.92), (GameConstants.DISPLAY_WIDTH, 50)
+        ),
+    )
 
     # Draw the walls
-    top_wall = pygame.Rect((wall_x_positions[current_wall_position], 0), (20, top_wall_length))
+    top_wall = pygame.Rect(
+        (wall_x_positions[current_wall_position], 0), (20, top_wall_length)
+    )
     top_wall = pygame.draw.rect(game_display, BLACK, top_wall)
 
-    bottom_wall_start = GameConstants.DISPLAY_HEIGHT - floor.height - bottom_wall_length  # Top of bottom wall
-    bottom_wall = pygame.Rect((wall_x_positions[current_wall_position], bottom_wall_start), (20, bottom_wall_length))
+    bottom_wall_start = (
+        GameConstants.DISPLAY_HEIGHT - floor.height - bottom_wall_length
+    )  # Top of bottom wall
+    bottom_wall = pygame.Rect(
+        (wall_x_positions[current_wall_position], bottom_wall_start),
+        (20, bottom_wall_length),
+    )
     bottom_wall = pygame.draw.rect(game_display, BLACK, bottom_wall)
 
     # Update flappy box
@@ -50,16 +69,21 @@ while True:
     # Check for collision with floor + walls
     if flappy_box.rect.colliderect(floor):
         pygame.quit()
-        print('SCORE: ', score)
-    elif flappy_box.rect.colliderect(top_wall) or flappy_box.rect.colliderect(bottom_wall):
+        print("SCORE: ", score)
+    elif flappy_box.rect.colliderect(top_wall) or flappy_box.rect.colliderect(
+        bottom_wall
+    ):
         pygame.quit()
-        print('SCORE: ', score)
+        print("SCORE: ", score)
 
     # If walls at end of screen, create new walls
     current_wall_position += 1
     if current_wall_position > len(wall_x_positions) - 1:
         index = random.randint(0, len(top_wall_lengths) - 1)
-        top_wall_length, bottom_wall_length = top_wall_lengths[index], bottom_wall_lengths[index]
+        top_wall_length, bottom_wall_length = (
+            top_wall_lengths[index],
+            bottom_wall_lengths[index],
+        )
 
     current_wall_position = current_wall_position % len(wall_x_positions)
 
